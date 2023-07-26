0fcb5fae31c4b2b5ff30a6eacbc748183d2d48ea
saralaf123 <saralafeta@gmail.com>
2023-07-26 13:10:32 +0100

    other modification

--- src/main/java/Game/EndGame.java
+++ src/main/java/Game/EndGame.java
@@ -2,64 +2,85 @@ package Game;
 
 import Players.Players;
 import Rules.Choice;
+import Rules.RpsChoice;
 
 import java.util.Scanner;
 
 public class EndGame {
-    private Integer winner1;
-    private Integer winner2;
-    public void setWinner1()
+    private int winner1;
+    private int winner2;
+    private boolean round;
+    public EndGame()
     {
-        this.winner1 =0;
+        round=true;
     }
 
-    public Integer getWinner1() {
+    public int getWinner1() {
         return this.winner1;
     }
-    public void setWinner2()
-    {
-        this.winner2 =0;
-    }
 
-    public Integer getWinner2() {
+
+    public int getWinner2() {
         return this.winner2;
     }
 
 
 
-  public String winner(Choice playerChoice1,Choice playerChoice2 ) {
+  public void winner(Choice choice1,Choice choice2) {
 
-
-      if (playerChoice1.equals(playerChoice2)) {
+      if (choice1.equals(choice2)) {
 
           System.out.println("no winner");
-      } else if (playerChoice1.equals("Rock") && playerChoice2.equals("Paper") || playerChoice1.equals("Scissor") && playerChoice2.equals("Rock") || playerChoice2.equals("Scissor") && playerChoice1.equals("Paper")) {
-
-          winner2++;
-          return " first player score "+winner1+" second player score "+winner2;
-
-
-      } else if(playerChoice2.equals("Rock") && playerChoice1.equals("Paper") || playerChoice2.equals("Scissor") && playerChoice1.equals("Rock") || playerChoice1.equals("Scissor") && playerChoice2.equals("Paper")) {
-
-          winner1++;
-          return " first player score "+winner1+" second player score "+winner2;
       }
-  return " first player score "+winner1+" second player score "+winner2;
+      else if (choice1.equals(RpsChoice.ROCK) && choice2.equals(RpsChoice.PAPER))
+      {
+          System.out.println("second player is the winner");
+          winner2++;
+      }
+      else if (choice1.equals(RpsChoice.PAPER) && choice2.equals(RpsChoice.ROCK))
+      {
+          System.out.println("first player is the winner");
+          winner1++;
+      }
+      else if (choice1.equals(RpsChoice.SCISSORS) && choice2.equals(RpsChoice.ROCK))
+      {
+          System.out.println("second player is the winner");
+          winner2++;
+      }
+      else if (choice1.equals(RpsChoice.ROCK) && choice2.equals(RpsChoice.SCISSORS))
+      {
+          System.out.println("first player is the winner");
+          winner1++;
+      }else if (choice1.equals(RpsChoice.SCISSORS) && choice2.equals(RpsChoice.PAPER))
+      {
+          System.out.println("first player is the winner");
+          winner1++;
+      } else if (choice2.equals(RpsChoice.SCISSORS) && choice1.equals(RpsChoice.PAPER))
+      {
+              System.out.println("second player is the winner");
+          winner2++;
+      }
+      else
+      {
+          System.out.println("Invalid");
+      }
+
   }
     public void StartGame(Players player1,Players player2)
     {
         Scanner scanner=new Scanner(System.in);
-        boolean round=true;
-        while (round=true)
+
+        while (round)
         {
             winner(player1.makeMove(),player2.makeMove());
             System.out.println("do you want to play another round yes/no?");
-            String asnwer=scanner.next();
-            if(asnwer.equals("no"))
+            String answer=scanner.next();
+            if(answer.equals("no"))
             {
                 round=false;
             }
 
         }
+        System.out.println("first player score "+getWinner1()+" the second player score "+getWinner2());
     }
 }
--- src/main/java/app.java
+++ src/main/java/app.java
@@ -7,9 +7,9 @@ public class app {
     public static void main(String[] args) {
 
         GamePlayer computerPlayer = new GamePlayer("Computer", "computer");
-       // Choice Move=computerPlayer.makeMove();
+
         GamePlayer humainPlayer = new GamePlayer("humain", "humain");
-       // computerPlayer.makeMove();
+
         EndGame game=new EndGame();
         game.StartGame(humainPlayer,computerPlayer);
 
